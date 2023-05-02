99758b2caeac79bba8b32e7009fc4f1e5f1176c2
Matt <73551894+osDreamscape@users.noreply.github.com>
2023-05-01 21:17:21 -0700

    Switched to VSC cuz Rider kept breaking, hope it doesn't break everything!

---
+++ Assets/Scripts/Model/PlayerParty.cs
@@ -0,0 +1,13 @@
+namespace DefaultNamespace; 
+
+internal class PlayerParty : AbstractParty {
+
+    private List<AbstractPlayerCharacter> playerPartyComposition;
+
+    PlayerParty(AbstractPlayerCharacter theHero) {
+        if (playerPartyComposition == null) {
+            playerPartyComposition = new List<AbstractPlayerCharacter>();
+            playerPartyComposition.
+        }
+    }
+}
