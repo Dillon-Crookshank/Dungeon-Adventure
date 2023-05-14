6c4d26d2d29cf6bcfdceab846588ca180bcc84a4
Matt <73551894+osDreamscape@users.noreply.github.com>
2023-05-13 23:03:26 -0700

    Some refactoring for Lists to Dictionaries, added move logic, toStrings for AbstractActor, consolidation of some behaviors upwards in the hierarchy, documentation,
    Game is more or less "playable" in console if input was allowed at this
    stage.

--- Assets/Scripts/Model/AbstractParty.cs
+++ Assets/Scripts/Model/AbstractParty.cs
@@ -9,27 +9,20 @@ internal abstract class AbstractParty
 {
 
     /// <summary>
+    /// The maximum party size allowed.
+    /// </summary>
+    public const int MAX_PARTY_SIZE = 6;
+
+    /// <summary>
     /// True if at least one party member is alive, false if all are killed.
     /// </summary>
-    private bool isAllAlive;
+    internal bool isAllAlive;
+
 
     /// <summary>
-    /// An enumeration to track the order of the party members.
+    /// A Dictionary that contains the AbstractActors that make up the party, and their positions.
     /// </summary>
-    private enum partyOrder //TODO: necessary???
-    {
-        partyMember1,
-        partyMember2,
-        partyMember3,
-        partyMember4,
-        partyMember5,
-        partyMember6
-    }
-
-    /// <summary>
-    /// A List that contains the AbstractActors that make up the party.
-    /// </summary>
-    private List<AbstractActor> partyComposition;
+    internal Dictionary<int, AbstractActor> partyPositions;
 
 
     /// <summary>
@@ -40,5 +33,59 @@ internal abstract class AbstractParty
     {
         return isAllAlive;
     }
+
+    /// <summary>
+    /// Adds an Actor into the party.
+    /// Party size must be positive, and max party size is <see cref=MAX_PARTY_SIZE>
+    /// </summary>
+    /// <param name="theActor">The Actor to be added to the party.</param>
+    /// <returns>True if added successfully, false otherwise.</returns>
+    internal bool AddActor(AbstractActor theActor)
+    {
+        int key = 0;
+        for (int i = 1; i <= MAX_PARTY_SIZE; i++)
+        {
+            if (!partyPositions.ContainsKey(i))
+            {
+                key = i;
+                break;
+            }
+        }
+        if (key == 0)
+        {
+            return false;
+        }
+        partyPositions.Add(key, theActor);
+        theActor.SetPartyPosition(key);
+        return true;
+    }
+
+    /// <summary>
+    /// Getter for the partyPositions Dictionary. 
+    /// </summary>
+    /// <returns>The AbstractActors in the party, mapped by the positions.</returns>
+    internal Dictionary<int, AbstractActor> GetPartyPositions()
+    {
+        return partyPositions;
+    }
+
+    /// <summary>
+    /// Moves an Actor into an open position in the party.
+    /// </summary>
+    /// <param name="thePosition">The party position the Actor is attempting to move to.</param>
+    /// <param name="theActor">The Actor attempting to move.</param>
+    /// <returns>True if move is successful, false otherwise.</returns>
+    internal bool moveCharacter(int thePosition, AbstractActor theActor)
+    {
+        if (thePosition < 1 || thePosition > 6 || partyPositions.ContainsKey(thePosition))
+        {
+            return false;
+        }
+        partyPositions.Remove(theActor.GetPartyPosition());
+        theActor.SetPartyPosition(thePosition);
+        partyPositions.Add(thePosition, theActor);
+        return true;
+    }
+
 
 }
---
+++ Assets/Scripts/Model/Debugging.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+
+    /// <summary>
+    /// A debugging class for testing purposes.
+    /// </summary>
+    internal class Debugging : MonoBehaviour
+    {
+        void Start()
+        {
+            testHero hero = new testHero("hero", 25, 5, 0, 10, 5);
+            testHero hero2 = new testHero("hero2", 25, 5, 0, 10, 5);
+            PlayerParty player = new PlayerParty(hero);
+            player.AddActor(hero2);
+            testEnemy enemy = new testEnemy("enemy", 25, 5, 0, 10, 5);
+            testEnemy enemy2 = new testEnemy("enemy2", 25, 5, 0, 10, 5);
+            EnemyParty enemies = new EnemyParty(enemy);
+            enemies.AddActor(enemy2);
+            List<AbstractActor> allChars = Combat.InitiativeRoll(player, enemies);
+            Debug.Log(allChars.Count);
+            allChars.Sort((x, y) => y.GetCombatInitiative() - x.GetCombatInitiative());
+            foreach (AbstractActor character in allChars)
+            {
+                Debug.Log(character.toString());
+            }
+        }
+    }
+}
